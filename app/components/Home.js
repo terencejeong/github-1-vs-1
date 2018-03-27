a9e9ac043de06d55b8a9d4a38b27b64a70664ef8
Terence Jeong <terence.jeong@gmail.com>
2018-03-27 13:42:56 +1100

    deployed!

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div className = "home-component">
-                <h1>Pit your Githubs 1 vs 1!</h1>
+                <h1>Pit your Githubs 1 vs 1! Hosted By Terry!</h1>
                 <Link className="button" to="/battle">
                     Battle
                 </Link>
